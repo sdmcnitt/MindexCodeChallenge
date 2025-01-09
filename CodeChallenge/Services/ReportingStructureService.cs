b2d30791780603f9e83f884016e0fec5ae672f32
sdmcnitt <sdmcnitt@hotmail.com>
2025-01-09 14:13:04 -0500

    bogus employee test passing for /reportingstructure

--- CodeChallenge/Services/ReportingStructureService.cs
+++ CodeChallenge/Services/ReportingStructureService.cs
@@ -7,17 +7,23 @@ namespace CodeChallenge.Services
     public class ReportingStructureService : IReportingStructureService
     {
         private readonly ILogger<ReportingStructureService> _logger;
+        private readonly IEmployeeService _employeeService;
 
-        public ReportingStructureService(ILogger<ReportingStructureService> logger)
+        public ReportingStructureService(ILogger<ReportingStructureService> logger,
+            IEmployeeService employeeService)
         {
             _logger = logger;
+            _employeeService = employeeService;
         }
 
         public ReportingStructure GetById(string id)
         {
             if(!String.IsNullOrEmpty(id))
             {
-                return new ReportingStructure() { };
+                var employee = _employeeService.GetById(id);
+                if(employee == null) return null;
+
+
             }
 
             return null;

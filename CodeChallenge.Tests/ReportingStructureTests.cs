2691286220928fdbc65fcb11823c3f995142c5ea
sdmcnitt <sdmcnitt@hotmail.com>
2025-01-09 13:09:45 -0500

    New ReportingStructureTests -- 404 as expected

---
+++ CodeChallenge.Tests/ReportingStructureTests.cs
@@ -0,0 +1,54 @@
+
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+
+using CodeChallenge.Models;
+
+using CodeCodeChallenge.Tests.Integration.Extensions;
+using CodeCodeChallenge.Tests.Integration.Helpers;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeCodeChallenge.Tests.Integration
+{
+    [TestClass]
+    public class ReportingControllerTests
+    {
+        private static HttpClient _httpClient;
+        private static TestServer _testServer;
+
+        [ClassInitialize]
+        // Attribute ClassInitialize requires this signature
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
+        public static void InitializeClass(TestContext context)
+        {
+            _testServer = new TestServer();
+            _httpClient = _testServer.NewClient();
+        }
+
+        [ClassCleanup]
+        public static void CleanUpTest()
+        {
+            _httpClient.Dispose();
+            _testServer.Dispose();
+        }
+
+        //TASK1: endpoint /api/reportingstructure
+        //TODO: add the endpoint test then create the endpoint to pass
+        [TestMethod]
+        public void GetReportingStructureById_Returns_Ok()
+        {
+            // Arrange
+            var employeeId = "16a596ae-edd3-4847-99fe-c4518e82c86f"; //john lennon
+
+            // Execute
+            var getRequestTask = _httpClient.GetAsync($"api/reportingstructure/{employeeId}");
+            var response = getRequestTask.Result;
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}

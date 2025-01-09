25b009dce165c590093edea1b7363195f1d122c6
sdmcnitt <sdmcnitt@hotmail.com>
2025-01-09 17:43:46 -0500

    TODO comment on ThenInclude for direct reports - test if this works for all levels down

--- CodeChallenge/Repositories/EmployeeRespository.cs
+++ CodeChallenge/Repositories/EmployeeRespository.cs
@@ -33,7 +33,7 @@ namespace CodeChallenge.Repositories
             //return _employeeContext.Employees.SingleOrDefault(e => e.EmployeeId == id);
             var employee = _employeeContext.Employees
                 .Include(e => e.DirectReports)
-                .ThenInclude(e => e.DirectReports)
+                .ThenInclude(e => e.DirectReports)   //TODO does this go to all levels of employee direct reports and on down -- create a test?
                 .SingleOrDefault(e => e.EmployeeId == id);
             return employee;
         }

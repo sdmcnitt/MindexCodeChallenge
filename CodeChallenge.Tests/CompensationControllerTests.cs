e06811c5656485c870c97949bf284d174b9db7f5
sdmcnitt <sdmcnitt@hotmail.com>
2025-01-09 16:58:50 -0500

    added new controller and test endpoint /compensation/id

---
+++ CodeChallenge.Tests/CompensationControllerTests.cs
@@ -0,0 +1,54 @@
+
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+
+using CodeChallenge.Models;
+
+using CodeCodeChallenge.Tests.Integration.Extensions;
+using CodeCodeChallenge.Tests.Integration.Helpers;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeCodeChallenge.Tests.Integration
+{
+    [TestClass]
+    public class CompensationControllerTests
+    {
+        private static HttpClient _httpClient;
+        private static TestServer _testServer;
+
+        [ClassInitialize]
+        // Attribute ClassInitialize requires this signature
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
+        public static void InitializeClass(TestContext context)
+        {
+            _testServer = new TestServer();
+            _httpClient = _testServer.NewClient();
+        }
+
+        [ClassCleanup]
+        public static void CleanUpTest()
+        {
+            _httpClient.Dispose();
+            _testServer.Dispose();
+        }
+
+        //TASK2: endpoint /api/compensation/id
+        //add the endpoint test then create the endpoint to pass
+        [TestMethod]
+        public void GetCompensationById_Returns_Ok()
+        {
+            // Arrange
+            var employeeId = "16a596ae-edd3-4847-99fe-c4518e82c86f"; //john lennon
+
+            // Execute
+            var getRequestTask = _httpClient.GetAsync($"api/compensation/{employeeId}");
+            var response = getRequestTask.Result;
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
---
+++ CodeChallenge/Controllers/CompensationController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace CodeChallenge.Controllers
+{
+    [ApiController]
+    [Route("api/compensation")]
+    public class CompensationController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public CompensationController(ILogger<CompensationController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCompensation(String id)
+        {
+            _logger.LogDebug($"Received compensation get request for '{id}'");
+
+            object compensation = "null";
+
+            if (compensation == null)
+                return NotFound();
+
+            return Ok(compensation);
+        }
+
+    }
+}
